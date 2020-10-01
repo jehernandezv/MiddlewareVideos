735ec53cb8a98186b1c20b1336bb3015f0bffded
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-09-30 19:07:06 -0500

    update

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
+const amqp = require('amqplib');
+const nameQueue = 'videos';
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
@@ -48,6 +50,31 @@ var videos = [
        "https://cnnespanol.cnn.com/wp-content/uploads/2019/12/s_64a163f16ecbb099e52f2f8271f73cbbfcfc9034be4d646f7375e4db1ca6f3d7_1573501883482_ap_19001106049831-1.jpg?quality=100&strip=info&w=320&h=240&crop=1"]
       }
 ]
+
+
+async function subscriber(){
+  const connection = await amqp.connect('amqp://localhost')
+  const channel = await connection.createChannel();
+
+  await channel.assertQueue(nameQueue);
+
+  const message = {
+    text:'hola'
+  }
+
+  const sent = await channel.sendToQueue(nameQueue, Buffer.from(
+    JSON.stringify(message)
+  ))
+
+    sent ?
+      console.log(`send message to "${nameQueue}"  queue ` , message) :
+      console.log(`Fails sending message to "${nameQueue}" queue ` , message)
+}
+
+subscriber().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
 
 app.get('/saludo', (req, res) => {
   res.json({
